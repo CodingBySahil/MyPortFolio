9059ed73f5fc9286b8d5447de1920da03d66b455
codingBySahil <muhammadsahil345737@gmail.com>
2024-07-05 10:37:12 +0500

    my portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
-import './styles/App.css';
+import './App.css';
 
 const App = () => {
   return (
     <div>
-      <h1>hello from app</h1>
       <Home />
       <About />
       <Projects />
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,5 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <h1>hello from main</h1>
   </React.StrictMode>,
 )
